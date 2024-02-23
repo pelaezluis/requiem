0c7d5ea8307b38404f9e3de2a62f34513302bd78
pelaezluis <luispelaezbedoya@gmail.com>
2024-02-23 17:45:15 -0500

    back to blogs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Home } from './pages/Home';
 
 function App() {
+
+  const [blogs, setBlogs] = useState([])
+  const url = 'http://localhost:5000/blogs'
+
+  const headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  }
+  const getBlogs = () => {
+    fetch(url, headers)
+    .then(res => res.json())
+    .then(res => setBlogs(res))
+  }
+  
+  useEffect(getBlogs, []) 
+
   return (
     <div className="App">
-      <Home />
+      <Home blogs={blogs}/>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,50 +4,21 @@ import '../assets/css/Home.css'
 import { Footer } from "../components/Footer";
 // import home_1 from '../assets/img/home_1.JPG'
 
-export function Home() {
-    const blogs = [
-        {
-            src: "https://i.pinimg.com/564x/5d/af/31/5daf31b72d1a2e83210f6b5003dd611b.jpg",
-            isNew: true
-        },
-        {
-            src: "https://i.pinimg.com/564x/cc/77/b0/cc77b076a1e833300a249215860c46c4.jpg"
-        },
-        {
-            src: "https://i.pinimg.com/564x/41/4d/01/414d01a0dee8eb05865ad395d6488d93.jpg"
-        },
-        {
-            src: "https://i.pinimg.com/564x/63/7c/16/637c1690e51d4184d13f52ba0a4ca2dc.jpg",
-            isNew: true
-        },
-        {
-            src: "https://i.pinimg.com/564x/c2/bd/91/c2bd91df03a0930aa12ead0eb1101119.jpg",
-            isNew: true
-        },
-        {
-            src: "https://i.pinimg.com/564x/cc/77/b0/cc77b076a1e833300a249215860c46c4.jpg"
-        },
-        {
-            src: "https://i.pinimg.com/564x/bd/93/73/bd9373772dfef750c911051bf48800a7.jpg"
-        },
-        {
-            src: "https://i.pinimg.com/564x/88/4e/67/884e67812734e2f78cba0e9f167694d0.jpg"
-        }
-    ]
+export function Home({ blogs }) {
 
     return (
         <div className="home">
             <Header />
-            {/* <div className="home-text-img">
-                <img className="home-img" src={home_1} alt="home_1" />
-            </div> */}
             <div className="home-blogs p-4">
                 {
-                    blogs.map( (blog, idx) => {
-                        return(
-                            <BlogCard key={idx} src={blog.src} isNew={blog.isNew} />
-                        )
-                    })
+                    blogs.length > 0 ? (
+                        blogs.map((blog, idx) => {
+                            return (
+                                <BlogCard key={idx} src={blog.src} isNew={blog.isNew} />
+                            )
+                        })
+                    ) : <p className="empty-blog">No hay entradas publicadas</p>
+                    
                 }
             </div>
             <Footer />
